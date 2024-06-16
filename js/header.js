38bbff1534a518b67c3177a43dababebed6329a8
Mackedonec <mackedonec187@gmail.com>
2024-06-16 17:46:14 +0300

    add burge

--- js/header.js
+++ js/header.js
@@ -1,10 +1,13 @@
 const burgerBtn = document.querySelector(".burger-btn");
+const burgerIcon = document.querySelector(".burger-icon");
 const navList = document.querySelector(".header-nav__list");
 
 burgerBtn.addEventListener("click", () => {
   navList.classList.toggle("nav-visible");
+  burgerIcon.classList.toggle("icon-cross");
 });
 
 navList.addEventListener("click", () => {
   navList.classList.remove("nav-visible");
+  burgerIcon.classList.toggle("icon-cross");
 });
